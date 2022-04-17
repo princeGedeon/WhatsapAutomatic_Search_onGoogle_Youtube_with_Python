2662138936aac3fd6459b62cb2b881ee9e91e34c
princeGedeon <guedjegedeon03@gmail.com>
2022-04-18 00:50:13 +0100

    Correction des bugs

--- api.py
+++ api.py
@@ -1,10 +1,56 @@
+import pywhatkit as kit
+import time
+import os
 def menu():
     print("-----------------MENU-----------------------------\n")
-    print("1..Ecris un texte en manuscrit (Obtiens son resultat sous forme d'image)")
+    print("1..Ecrire un texte en manuscrit (Obtiens son resultat sous forme d'image)")
     print("2..Recherche par mot clé")
     print("3..Recherche sur le navigateur")
     print('4..Envoyer un message Whatsapp à quelqu\'un')
     print('5..Recherche une video sur youtube')
+    print('6..Envoyer Message Whatsapp Instantannée')
+    print("7.. Quitter")
     return input("Quel action voulez-vous effectuez : ")
+
+def traitement(choix):
+    temp = choix
+    if temp == 1:
+        s = input("Entrez le texte à transformer en manuscrit : ")
+        kit.text_to_handwriting(s)
+    elif temp == 2:
+        s = input("Entrez le mot à Rechercher : ")
+        lines = input("Nombre de lignes : ")
+        lines = 3 if int(lines) <= 3 else int(lines)
+        try:
+            kit.info(s, lines=lines)
+
+        except:
+            print("Pas de resultats possibles")
+    elif temp == 3:
+        s = input("Mot a Rechercher : ")
+        kit.search(s)
+    elif temp == 4:
+        print("----ENVOI MESSAGE------")
+        num = input("Numero du destinataire (+xx xx xx xx xx) : ")
+        message = input("Message :")
+        h, m = list(map(int, input("Heure d'envoi (h:m):").split(':')))
+        kit.sendwhatmsg(num, message, h, m)
+    elif temp == 5:
+        print("---------Recherche Sur YouTUBE--------------")
+        s = input("Mot a rechercher sur youtube :")
+        kit.playonyt(s)
+    elif temp == 6:
+        print("---------------Message INSTANTANNEE--------")
+        num = input("Numero du destinataire (+xx xx xx xx xx) : ")
+        message = input("Message :")
+        kit.sendwhatmsg_instantly(num,message)
+    elif temp == 7:
+        print("Goodbye!!Bye bye")
+        return 0
+    else:
+        print('Action non Disponible.Veuillez Ressayer')
+
+    return 1
+
 
 
--- main.py
+++ main.py
@@ -1,30 +1,16 @@
-import pywhatkit as kit
+import time
 
-from api import menu
+from api import menu,traitement
 
 while True:
-    temp=menu()
-    if temp==1:
-        s=input("Entrez le texte à transformer en manuscrit : ")
-        kit.text_to_handwriting(s)
-    elif temp==2:
-        s=input("Entrez le mot à Rechercher : ")
-        lines=input("Nombre de lignes : ")
-        lines=3 if int(lines)<=3 else int(lines)
-        kit.info(s,lines=lines)
-    elif temp==3:
-        s=input("Mot a Rechercher : ")
-        kit.search(s)
-    elif temp==4:
-        print("----ENVOI MESSAGE------")
-        num=input("Numero du destinataire (+xx xx xx xx xx) : ")
-        message=input("Message :")
-        h,m=list(map(int,input("Heure d'envoi (h:m):").split(':')))
-        kit.sendwhatmsg(num,message,h,m)
-    elif temp==5:
-        print("---------Recherche Sur YouTUBE--------------")
-        s=input("Mot a rechercher sur youtube")
+    work=traitement(int(menu()))
+    print("Fin du traitement\n\n")
+    time.sleep(10)
 
+    if not work:
+        break
+    elif  int(input("Voulez vous continuez (0,1) : "))==0:
+        break
 
 #Transformer texte écrit en manuscrit
 #kit.text_to_handwriting('Hello world')
